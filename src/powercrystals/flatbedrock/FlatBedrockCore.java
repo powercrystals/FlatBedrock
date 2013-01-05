32f19d095e81220be68e5fc35a7c89893d48bf75
powercrystals <powcrystals@gmail.com>
2013-01-05 18:48:21 -0500

    Enabled auto-updated for FlatBedrock

--- src/powercrystals/flatbedrock/FlatBedrockCore.java
+++ src/powercrystals/flatbedrock/FlatBedrockCore.java
@@ -1,15 +1,20 @@
 package powercrystals.flatbedrock;
 
+import powercrystals.core.updater.IUpdateableMod;
+import powercrystals.core.updater.UpdateManager;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.TickRegistry;
+import cpw.mods.fml.relauncher.Side;
 
-@Mod(modid = FlatBedrockCore.modId, version = FlatBedrockCore.version, name = "FlatBedrock")
-public class FlatBedrockCore
+@Mod(modid = FlatBedrockCore.modId, version = FlatBedrockCore.version, name = FlatBedrockCore.modName)
+public class FlatBedrockCore implements IUpdateableMod
 {
 	public static final String modId = "FlatBedrock";
-	public static final String version = "1.4.6R1.0.0";
+	public static final String version = "1.4.6R1.0.1";
+	public static final String modName = "FlatBedrock";
 	
 	private static FlatBedrockCore _instance;
 	
@@ -25,5 +30,30 @@ public class FlatBedrockCore
 		
 		GameRegistry.registerWorldGenerator(new FlatBedrockWorldGen());
 		
+		TickRegistry.registerScheduledTickHandler(new UpdateManager(this), Side.CLIENT);
+	}
+
+	@Override
+	public String getModId()
+	{
+		return modId;
+	}
+
+	@Override
+	public String getModName()
+	{
+		return modName;
+	}
+
+	@Override
+	public String getModFolder()
+	{
+		return modId;
+	}
+
+	@Override
+	public String getModVersion()
+	{
+		return version;
 	}
 }
