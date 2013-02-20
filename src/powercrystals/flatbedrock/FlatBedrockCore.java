5852d062eaf3f49d1df960934417df7ba4437182
LazDude2012 <alex.j.fischer@gmail.com>
2013-02-19 17:12:33 -0700

    Add config file.
    Adds a boolean value to a config file, specifying whether the mod carries through on its promise of flat bedrock.

--- src/powercrystals/flatbedrock/FlatBedrockCore.java
+++ src/powercrystals/flatbedrock/FlatBedrockCore.java
@@ -3,6 +3,8 @@ package powercrystals.flatbedrock;
 import powercrystals.core.updater.IUpdateableMod;
 import powercrystals.core.updater.UpdateManager;
 import cpw.mods.fml.common.Mod;
+import net.minecraftforge.Configuration;
+import net.minecraftforge.Property;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -15,6 +17,7 @@ public class FlatBedrockCore implements IUpdateableMod
 	public static final String modId = "FlatBedrock";
 	public static final String version = "1.4.6R1.0.3";
 	public static final String modName = "FlatBedrock";
+	public static bool reallyGenerate = false;
 	
 	private static FlatBedrockCore _instance;
 	
@@ -23,14 +26,29 @@ public class FlatBedrockCore implements IUpdateableMod
 		return _instance;
 	}
 	
+	@Mod.PreInit
+	public void preload(FMLPreInitializationEvent e)
+	{
+		Configuration config = new Configuration(e.getSuggestedConfigurationFile());
+		config.load();
+		Property doGenerate;
+		doGenerate = config.get("WorldGen","reallyFlattenBedrock",true);
+		reallyGenerate = doGenerate.getBool();		
+	}
+	
 	@Init
 	public void load(FMLInitializationEvent e)
 	{
 		_instance = this;
 		
+		if(reallyGenerate)
+		{
+		
 		GameRegistry.registerWorldGenerator(new FlatBedrockWorldGen());
 		
 		TickRegistry.registerScheduledTickHandler(new UpdateManager(this), Side.CLIENT);
+		
+		}
 	}
 
 	@Override
